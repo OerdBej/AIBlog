4f671e573fd9b233c3d5f1ae87e2f9e818032ba0
oerdbej <oerdbej@gmail.com>
2025-06-11 13:12:58 +0200

    adding key for div

--- frontend/src/StoriesList.tsx
+++ frontend/src/StoriesList.tsx
@@ -15,7 +15,10 @@ export default function StoriesList({ stories }: StoriesProps) {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
       {stories.map((story) => (
-        <div className="bg-[#309898] text-white px-4 py-6 flex flex-col gap-y-4 rounded">
+        <div
+          key={story.name}
+          className="bg-[#309898] text-white px-4 py-6 flex flex-col gap-y-4 rounded"
+        >
           <h3 className="font-bold text-xl">{story.title}</h3>
           {/* content is a key with value of array that holds string. iterate over,get the first paragraph and limit by characters */}
           <p>{story.content[0].substring(0, 100) + "..."}</p>
