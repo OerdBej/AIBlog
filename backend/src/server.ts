9e304c887b55b60f17a400bc4ef9d694e1d2c56d
oerdbej <oerdbej@gmail.com>
2025-06-13 14:23:03 +0200

    comments work

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
+app.use(express.json()); //user sended a obj so we handle it
 
 interface Comment {
   writtenBy: string;
@@ -37,14 +38,15 @@ app.post("/api/stories/:name/like", (req: Request, res: Response) => {
 
 // comments increase
 app.post("/api/stories/:name/comments", (req: Request, res: Response) => {
-  const { writtenBy, content } = req.body;
+  const { writtenBy, content } = req.body; //json confg bcs we send objt
   const story = stories.find((story) => story.name === req.params.name);
   // comments is an array and we add obj so we can know how wrote/and content
   story!.comments.push({
     writtenBy,
     content,
   });
-  res.send(`Story ${req.params.name} has totally ${story!.likes}`);
+  //sending a json req back to user - parse to JS obje to get out data
+  res.json(story);
 });
 
 app.listen(3000, () => {
