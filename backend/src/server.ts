28c9b54f152dad276675c0133d06f9c8b660d300
oerdbej <oerdbej@gmail.com>
2025-06-16 12:39:48 +0200

    mongo database - create colletion

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from "express";
+import { Collection, Db, MongoClient, ServerApiVersion } from "mongodb";
 
 const app: Express = express();
 app.use(express.json()); //user sended a obj so we handle it
@@ -22,6 +23,27 @@ const stories: Story[] = [
   { name: "Mindset", likes: 0, comments: [] },
   { name: "Switch", likes: 0, comments: [] },
 ];
+
+let db: Db;
+let dbcollection: Collection<Story>;
+
+//port of adress
+async function dbConnection() {
+  const uri = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  await client.connect();
+  db = client.db("mern-journal");
+
+  //name of the collection
+  dbcollection = db.collection("stories");
+}
 
 app.get("/stories/:name", (req: Request, res: Response) => {
   const { name } = req.params;
@@ -49,6 +71,12 @@ app.post("/api/stories/:name/comments", (req: Request, res: Response) => {
   res.json(story);
 });
 
-app.listen(3000, () => {
-  console.log("Server is up BRO");
+// Connect to MongoDB before starting the server
+dbConnection().then(() => {
+  app.listen(3000, () => {
+    console.log("Server is up BRO");
+    console.log("MongoDB connection established");
+  });
+}).catch(err => {
+  console.error("Failed to connect to MongoDB:", err);
 });
