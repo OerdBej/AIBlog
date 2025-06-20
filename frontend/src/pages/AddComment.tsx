b72dd1e03b586eb5e1f0957077b74879e8737777
oerdbej <oerdbej@gmail.com>
2025-06-20 12:01:11 +0200

    refactor AddComment component to handle comment submission and improve form structure

--- frontend/src/pages/AddComment.tsx
+++ frontend/src/pages/AddComment.tsx
@@ -12,10 +12,24 @@ interface CommentStoryProp {
 
 export default function AddComment({ commentStory }: CommentStoryProp) {
   const [nameText, setNameText] = useState("");
-  const [commentText, setCommentText] = useState("");
+  const [contentText, setContentText] = useState("");
+
+  const handleCommenting = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!nameText || !contentText) return;
+    commentStory({ nameText, contentText });
+    setNameText("");
+    setContentText("");
+  };
+
   return (
     <>
-      <h3>for the comment</h3>
+      <form
+        className="flex flex-col gap-8 self-center w-full text-white lg:max-w-3xl my-8"
+        onSubmit={handleCommenting}
+      >
+        <h3 className="text-2xl font-bold mb-4">here comment</h3>
+      </form>
     </>
   );
 }
