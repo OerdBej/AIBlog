bb2060cf53a6a77576d92d7bcc2b6fdf110105e7
oerdbej <oerdbej@gmail.com>
2025-06-20 12:20:29 +0200

    add input field for name in AddComment component

--- frontend/src/pages/AddComment.tsx
+++ frontend/src/pages/AddComment.tsx
@@ -29,6 +29,20 @@ export default function AddComment({ commentStory }: CommentStoryProp) {
         onSubmit={handleCommenting}
       >
         <h3 className="text-2xl font-bold mb-4">here comment</h3>
+        <label
+          className="flex flex-col text-xl gap-2 focus-within:text-[#FF6500] focus-within:font-bold mb-8"
+          htmlFor="name"
+        >
+          Name
+          <input
+            className="border-b border-gray-300 focus:border-[#bc9c41] outline-none"
+            type="text"
+            id="name"
+            value={nameText}
+            onChange={(e) => setNameText(e.target.value)}
+            required
+          />
+        </label>
       </form>
     </>
   );
