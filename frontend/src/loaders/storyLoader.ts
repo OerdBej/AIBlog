ab5798560712e7bc98bc3235413f8860c91d87c9
oerdbej <oerdbej@gmail.com>
2025-07-09 21:51:14 +0200

    error handling for storyloader if apicall failed

--- frontend/src/loaders/storyLoader.ts
+++ frontend/src/loaders/storyLoader.ts
@@ -2,7 +2,12 @@ import axios from "axios";
 import type { LoaderFunctionArgs } from "react-router";
 
 export async function storyLoader({ params }: LoaderFunctionArgs) {
-  const response = await axios.get(`/api/stories/${params.name}`);
-  const { likes, comments } = response.data;
-  return { likes, comments };
+  try {
+    const response = await axios.get(`/api/stories/${params.name}`);
+    const { likes, comments } = response.data;
+    return { likes, comments };
+  } catch (error) {
+    console.log("Error loading story data", error);
+    return { likes: 0, comments: [] };
+  }
 }
