a3b167c2cb6ae3324ceeedbdec124f74043a5ed2
oerdbej <oerdbej@gmail.com>
2025-07-12 11:45:18 +0200

    react router with the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import React from "react";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Blog from "./pages/Blog";
+import Navbar from "./components/Navbar";
+
+const App = () => {
   return (
-    <>
-      <h4>AI Blogger for Books</h4>
-    </>
+    <div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
+      <img
+        src={assets.logo}
+        alt="logo for the project"
+        className="w-6 h-6 sm:w-24 sm:h-24"
+      />
+      <button className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-emerald-700 text-white px-10 py-2.5 ">
+        Login
+        <img src={assets.arrow} alt="the logo" className="w-2 h-1.5" />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
