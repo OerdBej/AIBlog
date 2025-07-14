5b661fac4ff7ef4faef8a78d0f239f8c644ca5e5
oerdbej <oerdbej@gmail.com>
2025-07-14 21:17:16 +0200

    refactor: enhance header section layout and styling

--- frontend/src/components/HeaderSection.jsx
+++ frontend/src/components/HeaderSection.jsx
@@ -5,11 +5,26 @@ const HeaderSection = () => {
   return (
     <main className="border-1 border-red-600 mx-8 sm:mx-16 relative">
       {/* AI features part */}
-      <div className="text-center mt-20 mb-8">
+      <div className="text-center mt-20 mb-8 border-1 border-green-500">
         <div className="border border-primary/40 bg-primary/10 rounded-full text-sm text-primary inline-flex items-center gap-4 px-6 py-2 mb-4  ">
           <p>Generate your reviews using AI features</p>
           <img src={assets.star_icon} className="w-2.5" alt="icon" />
         </div>
+
+        <h1 className="text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700">
+          Welcome to <br />
+          <span className="text-primary"> Book Club</span>
+        </h1>
+        <p className="my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs">
+          Our book club blog is a cozy spot for book lovers to connect, chat
+          about their favorite reads, and get lost in lively, fun discussions
+          about stories that spark joy and inspiration. make it shorter
+        </p>
+
+        <form>
+          <input type="text" name="search for books" required />
+          <button type="submit">Search</button>
+        </form>
       </div>
       {/* Gradient of the page */}
       <img
