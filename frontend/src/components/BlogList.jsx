888fd50d0afc46bf65052f2e0cc0463e3b937693
oerdbej <oerdbej@gmail.com>
2025-07-15 22:12:34 +0200

    filtering blogs based on tag category' '

--- frontend/src/components/BlogList.jsx
+++ frontend/src/components/BlogList.jsx
@@ -30,9 +30,9 @@ const BlogList = () => {
         ))}
       </div>
       {/* items */}
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
         {blog_data
-          .filter((blog) => (menu === "ALL" ? true : blog.category === menu))
+          .filter((blog) => (menu === "All" ? true : blog.category === menu))
           .map((blog) => (
             <BlogCard key={blog._id} blog={blog} />
           ))}
