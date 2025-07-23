943d797361e36fe8b72852839698ba3443fa073b
oerdbej <oerdbej@gmail.com>
2025-07-23 21:50:33 +0200

    adding blog data for the params of articles - fetching using useEffect when loaded

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { blog_data } from "../assets/assets";
 
 const Blog = () => {
-  return (
+  const { id } = useParams();
+
+  //get all blog data
+  const [data, setData] = useState(null);
+
+  const fetchBlogData = async () => {
+    // if data is = to params
+    const data = blog_data.find((item) => item._id === id);
+    setData(data);
+  };
+
+  //component loaded get data
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+
+  return data ? (
     <div>
-      <h3>this is the blog page</h3>
+      <h3>This is data</h3>
     </div>
+  ) : (
+    <h3>Loading</h3>
   );
 };
 
