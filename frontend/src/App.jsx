33d62126f4bcbfb433874a8c21e8af13de0382ca
oerdbej <oerdbej@gmail.com>
2025-08-03 12:35:24 +0200

    Fix LayoutAdmin component to properly render and add logout functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Blog from "./pages/Blog";
 import NewsLetter from "./components/NewsLetter";
 import LayoutAdmin from "./pages/admin/LayoutAdmin";
 import Dashboard from "./pages/admin/Dashboard";
+
 import AddBlog from "./pages/admin/AddBlog";
 import BlogList from "./components/BlogList";
 import Comments from "./pages/admin/Comments";
@@ -16,7 +17,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/blog/:id" element={<Blog />} />
           {/* with child routes: //click admin -> open LayoutAdmin -> add the Dashboard */}
-          <Route path="/admin" element={LayoutAdmin}>
+          <Route path="/admin" element={<LayoutAdmin />}>
             <Route index element={<Dashboard />} />
             <Route path="addBlog" element={AddBlog} />
             <Route path="listBlog" element={BlogList} />
--- frontend/src/pages/admin/LayoutAdmin.jsx
+++ frontend/src/pages/admin/LayoutAdmin.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const LayoutAdmin = () => {
-  return <div>LayoutAdmin</div>;
+  const navigate = useNavigate();
+
+  const logout = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <div className="flex items-center justify-between py-2 h-[70x] px-4 sm:px-12 border-b border-gray-200">
+        <img
+          src={assets.logo}
+          alt="the logo"
+          className="w-30 sm:w-19 cursor-pointer "
+          onClick={() => navigate("/")}
+        />
+        <button
+          onClick={logout}
+          className="text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer"
+        >
+          Logout
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default LayoutAdmin;
