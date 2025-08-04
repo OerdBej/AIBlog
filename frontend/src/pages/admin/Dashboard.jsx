fa28e1f42162e1fcd588c245f8830198df523fc4
oerdbej <oerdbej@gmail.com>
2025-08-04 23:46:45 +0200

    Implement Dashboard component with state management and data fetching

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { assets, dashboard_data } from "../../assets/assets";
 
 const Dashboard = () => {
-  return <div> </div>;
+  const [dashboardData, setDashboardData] = useState({
+    blogs: 0,
+    comments: 0,
+    drafts: 0,
+    recentBlogs: [],
+  });
+
+  // components get load
+
+  const fetchDashoard = async () => {
+    setDashboardData(dashboard_data);
+  };
+
+  useEffect(() => {
+    fetchDashoard();
+  }, []);
+
+  return (
+    <div className='flex-1 p-4 md:p-10 bg-blue-50/50'>
+      {/* 3 cards row */}
+      <div className='flex flex-wrap gap-4'>
+        <div>
+          <img src={assets.dashboard_icon_1} alt='the icon' />
+          <div>
+            {/* state for the dashboard to hold */}
+            <p>{dashboardData.blogs}</p>
+            <p>Blogs</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
