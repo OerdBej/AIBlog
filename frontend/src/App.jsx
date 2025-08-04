759295b87c23d32bc43424536e71913c23ec2223
oerdbej <oerdbej@gmail.com>
2025-08-04 20:21:24 +0200

    Implement Login component with form handling and styling - before control input fields

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from "./pages/admin/Dashboard";
 import AddBlog from "./pages/admin/AddBlog";
 import BlogList from "./components/BlogList";
 import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
 
 const App = () => {
   return (
@@ -16,8 +17,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog/:id" element={<Blog />} />
-          {/* with child routes: //click admin -> open LayoutAdmin -> add the Dashboard */}
-          <Route path="/admin" element={<LayoutAdmin />}>
+          {/* with child routes: //click admin -> open LayoutAdmin -> add the Dashboard - if the user is log in display it if not layout*/}
+          <Route path="/admin" element={false ? <LayoutAdmin /> : <Login />}>
             <Route index element={<Dashboard />} />
             <Route path="addBlog" element={AddBlog} />
             <Route path="listBlog" element={BlogList} />
