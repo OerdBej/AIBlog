52e56fd4f3d1aeeea3ac032c1c5425b2939e218e
oerdbej <oerdbej@gmail.com>
2025-08-07 21:28:45 +0200

    Add CommentTableItem component and integrate it into Comments page for displaying comments

--- frontend/src/pages/admin/Comments.jsx
+++ frontend/src/pages/admin/Comments.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { comments_data } from "../../assets/assets";
+import CommentTableItem from "../../components/admin/CommentTableItem";
 
 const Comments = () => {
   const [comments, setComments] = useState([]);
@@ -19,6 +20,7 @@ const Comments = () => {
         <h1>Your comments</h1>
         <div className='flex gap-4'>
           <button
+            onClick={() => setFilter("Approved")}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
               filter === "Approved" ? "text-primary" : "text-gray-700"
             }`}
@@ -26,6 +28,7 @@ const Comments = () => {
             Approved
           </button>{" "}
           <button
+            onClick={() => setFilter("Not Approved")}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
               filter === "Not Approved" ? "text-primary" : "text-gray-700"
             }`}
@@ -33,6 +36,40 @@ const Comments = () => {
             Not Approved
           </button>
         </div>
+      </div>
+      {/* not approved */}
+      <div className='relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg scrollbar-hide'>
+        <table className='w-full text-sm text-gray-500'>
+          <thead className='text-sm text-gray-700 text-left uppercase'>
+            <tr>
+              <th scope='col' className='px-6 py-3 '>
+                Blog Title & Comment
+              </th>{" "}
+              <th scope='col' className='px-6 py-3 max-sm:hidden '>
+                Date{" "}
+              </th>{" "}
+              <th scope='col' className='px-6 py-3 '>
+                Actions
+              </th>
+            </tr>
+          </thead>
+          {/* display table data */}
+          <tbody>
+            {comments
+              .filter((comment) => {
+                if (filter === "Approved") return comment.isApproved === true;
+                return comment.isApproved === false;
+              })
+              .map((comment, index) => (
+                <CommentTableItem
+                  key={comment._id}
+                  comment={comment}
+                  index={index + 1}
+                  fetchComments={fetchComments}
+                />
+              ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
