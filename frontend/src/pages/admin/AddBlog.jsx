6456af4eba14efd174875dcf3750a5bd5088f819
oerdbej <oerdbej@gmail.com>
2025-08-08 11:40:45 +0200

    display thumbnail state image

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 
 const AddBlog = () => {
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [category, setCategory] = useState("Startup");
+  const [isPublished, setIsPublished] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className='flex bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+    <form
+      onSubmit={submitHandler}
+      className='flex bg-blue-50/50 text-gray-600 h-full overflow-scroll'
+    >
       <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:sm-10 shadow rounded'>
         <p>Upload thumbnail</p>
         <label htmlFor='image'>
-          <img src={assets.upload_area} alt='' />
+          <img
+            // 💡 image state is OK display it & or display thumbnail
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            alt=''
+          />
         </label>
         <input
+          // selecting the first image from input field
+          onChange={(e) => setImage(e.target.files[0])}
           type='file'
           id='image'
           required
