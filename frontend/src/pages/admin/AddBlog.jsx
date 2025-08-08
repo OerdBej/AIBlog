df5ea536d0313c606873e1d8f9ba02f443877cab
oerdbej <oerdbej@gmail.com>
2025-08-08 11:31:00 +0200

    adding the upload thumbnail

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const AddBlog = () => {
-  return <div>AddBlog</div>;
+  return (
+    <form className='flex bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+      <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:sm-10 shadow rounded'>
+        <p>Upload thumbnail</p>
+        <label htmlFor='image'>
+          <img src={assets.upload_area} alt='' />
+        </label>
+        <input
+          type='file'
+          id='image'
+          required
+          hidden
+          className='mt-2 h-16 rounded cursor-pointer'
+        />
+      </div>
+    </form>
+  );
 };
 
 export default AddBlog;
