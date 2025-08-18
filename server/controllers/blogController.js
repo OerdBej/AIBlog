1b327fe6808c68ace497367cc144412de0bc29da
oerdbej <oerdbej@gmail.com>
2025-08-18 20:16:25 +0200

    adding the image optimization to the mongoDb model

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,7 +1,8 @@
 import fs from "fs";
 import imagekit from "../configs/imageKit.js";
+import Blog from "../models/Blog.js";
 
-// in order to add the new data
+// in order to add the new data from our admin dashboard we send all the data
 export const addBlog = async (req, res) => {
   try {
     const { title, subTitle, description, category, isPublished } = JSON.parse(
@@ -16,13 +17,16 @@ export const addBlog = async (req, res) => {
 
     // upload the image after converting it in buffer format => mandatory from imageKit documenation
     const fileBuffer = fs.readFileSync(imageFile.path);
+
+    // from this response will get the url property for the link of our uploaded image
     const response = await imagekit.upload({
       file: fileBuffer,
       fileName: imageFile.originalname,
       folder: "/blogs",
     });
 
-    // optimization through imageKit
+    // optimization through imageKit => store it to a variable
+
     const optimizedImageUrl = imagekit.url({
       path: response.filePath,
       transformation: [
@@ -31,5 +35,19 @@ export const addBlog = async (req, res) => {
         { width: "1280" },
       ],
     });
-  } catch (error) {}
+
+    const image = optimizedImageUrl;
+    // save to mongoDB model
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+    res.json({ success: true, message: "Blog added successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 };
