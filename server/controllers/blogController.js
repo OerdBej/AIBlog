06251e99d890ebf03e4f8e8403ab3f91ad90f7ba
oerdbej <oerdbej@gmail.com>
2025-08-18 18:47:28 +0200

    upload image and optimize image thorugh imageKit URL transformation

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,15 +1,33 @@
+import fs from "fs";
+import imagekit from "../configs/imageKit.js";
 export const addBlog = async (req, res) => {
   try {
-    // when we add a new blog post we send also the data  (title, subtitle etc ) and provide the image
-
     const { title, subTitle, description, category, isPublished } = JSON.parse(
       req.body.blog
     );
-
+    // parse it from middleware
     const imageFile = req.file;
 
     if (!title || !description || !category || !imageFile) {
       return res.json({ success: false, message: "Missing fields" });
     }
+
+    // upload the image after converting it in buffer format => mandatory from imageKit documenation
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/blogs",
+    });
+
+    // optimization through imageKit
+    const optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { quality: "auto" },
+        { format: "webp" },
+        { width: "1280" },
+      ],
+    });
   } catch (error) {}
 };
