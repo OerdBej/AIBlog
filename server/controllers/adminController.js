c9a8896c0018fd5b1a2ab04cc0df64a81eb166bd
oerdbej <oerdbej@gmail.com>
2025-09-02 23:31:08 +0200

    admin to get all the comments and the blogs available from user

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,7 @@
 import jwt from 'jsonwebtoken';
+import Blog from '../models/Blog.js';
+import Comment from '../models/Comment.js';
+
 // get the data AND verify email/pass so we can get auth token
 export const adminLogin = async (req, res) => {
   try {
@@ -12,6 +15,29 @@ export const adminLogin = async (req, res) => {
     //send the token to response
     const token = jwt.sign({ email }, process.env.JWT_SECRET);
     res.json({ success: true, token });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// admin can get blog list  = find all the blog post wether isPublish is true or false
+export const getAllBlogsAdmin = async (req, res) => {
+  try {
+    // get all the blogs and sort them
+    const blogs = await Blog.find({}).sort({ createdAt: -1 });
+    res.json({ success: true, blogs });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//admin can see all comments if they are approved. find and populate with our data
+export const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({})
+      .populate('blog')
+      .sort({ createdAt: -1 });
+    res.json({ success: true, comments });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {
   addBlog,
+  addComment,
   deleteBlogById,
   getAllBlogs,
   getBlogById,
+  getBlogComments,
   togglePublish,
 } from '../controllers/blogController.js';
 import upload from '../middleware/multer.js';
@@ -18,5 +20,9 @@ blogRouter.get('/:blogId', getBlogById);
 // only admins can do it => so we protect it with AUTH
 blogRouter.post('/delete', auth, deleteBlogById);
 blogRouter.post('/toggle-publish', auth, togglePublish);
+
+// comments
+blogRouter.post('/add-comment', addComment);
+blogRouter.post('/comments', getBlogComments);
 
 export default blogRouter;
